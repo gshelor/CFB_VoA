316b8e16132c32e52abd93e4763e669a3b91e850
Griffin Shelor <griffinshelor@gmail.com>
2025-11-18 19:53:08 -0600

    week 13 projections

--- Scripts/R/CFB_VoA_ModelEval.R
+++ Scripts/R/CFB_VoA_ModelEval.R
@@ -194,7 +194,7 @@ if (as.numeric(cfb_week) >= 4){
     ggtitle(paste(season, "Correct Predicted Winner (%) by Week"), subtitle = "Vegas Metric in Blue") +
     theme(plot.title = element_text(size = 35, hjust = 0.5), plot.subtitle = element_text(hjust = 0.5), axis.text.x = element_text(size = 20), axis.text.y = element_text(size = 20), axis.title.x = element_text(size = 22), axis.title.y = element_text(size = 22), legend.text = element_text(size = 20))
   
-  ### diplaying plots
+  ### displaying plots
   WeeklyMAEPlot
   WeeklyRMSEPlot
   WeeklyWinPctPlot
